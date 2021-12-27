93cfe2ee11131edff3cf8f153ccab6f2a4f522f4
Aurimas Niekis <aurimas@niekis.lt>
2021-12-27 21:02:48 +0200

    Changed AttributesCompiler to use serializer/unserializer when compiling Attribute arguments instead of var_export. This should fix #46 issue

---
+++ tests/Fixture/Attribute/NestedAttribute.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CuyZ\Valinor\Tests\Fixture\Attribute;
+
+use Attribute;
+
+#[Attribute]
+final class NestedAttribute
+{
+    public function __construct(
+        /** @var list<object> */
+        public array $nestedAttributes,
+    ) {
+    }
+}
---
+++ tests/Fixture/Object/ObjectWithNestedAttributes.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CuyZ\Valinor\Tests\Fixture\Object;
+
+use CuyZ\Valinor\Tests\Fixture\Attribute\AttributeWithArguments;
+use CuyZ\Valinor\Tests\Fixture\Attribute\BasicAttribute;
+use CuyZ\Valinor\Tests\Fixture\Attribute\NestedAttribute;
+
+#[BasicAttribute]
+#[AttributeWithArguments('foo', 'bar')]
+#[NestedAttribute([
+    new BasicAttribute(),
+    new AttributeWithArguments('foo', 'bar'),
+])]
+// @PHP8.0 move to anonymous class
+final class ObjectWithNestedAttributes
+{
+    #[BasicAttribute]
+    #[AttributeWithArguments('foo', 'bar')]
+    #[NestedAttribute([
+        new BasicAttribute(),
+        new AttributeWithArguments('foo', 'bar'),
+    ])]
+    public bool $property;
+
+    #[BasicAttribute]
+    #[AttributeWithArguments('foo', 'bar')]
+    #[NestedAttribute([
+        new BasicAttribute(),
+        new AttributeWithArguments('foo', 'bar'),
+    ])]
+    public function method(#[BasicAttribute] string $parameter): void
+    {
+    }
+}
