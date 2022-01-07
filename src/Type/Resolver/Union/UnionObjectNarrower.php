ae68c3a5c5997be47c6295cd9677627524a4fb9c
Romain Canon <romain.hydrocanon@gmail.com>
2022-01-07 13:35:10 +0100

    feat: introduce automatic union of objects inferring during mapping
    When the mapper needs to map a source to a union of objects, it will try
    to guess which object it will map to, based on the needed arguments of
    the objects, and the values contained in the source.
    
    ```php
    final class UnionOfObjects
    {
        public readonly SomeFooObject|SomeBarObject $object;
    }
    
    final class SomeFooObject
    {
        public readonly string $foo;
    }
    
    final class SomeBarObject
    {
        public readonly string $bar;
    }
    
    // Will map to an instance of `SomeFooObject`
    (new \CuyZ\Valinor\MapperBuilder())
        ->mapper()
        ->map(UnionOfObjects::class, ['foo' => 'foo']);
    
    // Will map to an instance of `SomeBarObject`
    (new \CuyZ\Valinor\MapperBuilder())
        ->mapper()
        ->map(UnionOfObjects::class, ['bar' => 'bar']);
    ```

---
+++ src/Type/Resolver/Union/UnionObjectNarrower.php
@@ -0,0 +1,116 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CuyZ\Valinor\Type\Resolver\Union;
+
+use CuyZ\Valinor\Definition\Repository\ClassDefinitionRepository;
+use CuyZ\Valinor\Mapper\Object\Argument;
+use CuyZ\Valinor\Mapper\Object\Factory\ObjectBuilderFactory;
+use CuyZ\Valinor\Type\ObjectType;
+use CuyZ\Valinor\Type\Resolver\Exception\CannotResolveObjectTypeFromUnion;
+use CuyZ\Valinor\Type\Type;
+use CuyZ\Valinor\Type\Types\UnionType;
+
+use function array_map;
+use function array_pop;
+use function array_shift;
+use function count;
+use function in_array;
+use function is_array;
+use function ksort;
+
+final class UnionObjectNarrower implements UnionNarrower
+{
+    private UnionNarrower $delegate;
+
+    private ClassDefinitionRepository $classDefinitionRepository;
+
+    private ObjectBuilderFactory $objectBuilderFactory;
+
+    public function __construct(
+        UnionNarrower $delegate,
+        ClassDefinitionRepository $classDefinitionRepository,
+        ObjectBuilderFactory $objectBuilderFactory
+    ) {
+        $this->delegate = $delegate;
+        $this->classDefinitionRepository = $classDefinitionRepository;
+        $this->objectBuilderFactory = $objectBuilderFactory;
+    }
+
+    public function narrow(UnionType $unionType, $source): Type
+    {
+        if (! is_array($source)) {
+            return $this->delegate->narrow($unionType, $source);
+        }
+
+        $isIncremental = true;
+        $types = [];
+        $argumentsList = [];
+
+        foreach ($unionType->types() as $type) {
+            if (! $type instanceof ObjectType) {
+                return $this->delegate->narrow($unionType, $source);
+            }
+
+            $class = $this->classDefinitionRepository->for($type->signature());
+            $objectBuilder = $this->objectBuilderFactory->for($class);
+            $arguments = [...$objectBuilder->describeArguments()];
+
+            foreach ($arguments as $argument) {
+                if (! isset($source[$argument->name()]) && $argument->isRequired()) {
+                    continue 2;
+                }
+            }
+
+            $count = count($arguments);
+
+            if (isset($types[$count])) {
+                $isIncremental = false;
+                /** @infection-ignore-all */
+                break;
+            }
+
+            $types[$count] = $type;
+            $argumentsList[$count] = $arguments;
+        }
+
+        ksort($types);
+        ksort($argumentsList);
+
+        if ($isIncremental && count($types) >= 1 && $this->argumentsAreSharedAcrossList($argumentsList)) {
+            return array_pop($types);
+        }
+
+        throw new CannotResolveObjectTypeFromUnion($unionType);
+    }
+
+    /**
+     * @param array<int, array<Argument>> $argumentsList
+     */
+    private function argumentsAreSharedAcrossList(array $argumentsList): bool
+    {
+        $namesList = [];
+
+        foreach ($argumentsList as $arguments) {
+            $namesList[] = array_map(fn (Argument $argument) => $argument->name(), $arguments);
+        }
+
+        while ($current = array_shift($namesList)) {
+            if (count($namesList) === 0) {
+                /** @infection-ignore-all */
+                break;
+            }
+
+            foreach ($current as $name) {
+                foreach ($namesList as $other) {
+                    if (! in_array($name, $other, true)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
