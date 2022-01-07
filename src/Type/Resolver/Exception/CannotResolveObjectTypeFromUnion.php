79d7c266ecabc069b11120338ae1e62a8f3dca97
Romain Canon <romain.hydrocanon@gmail.com>
2022-01-07 13:35:10 +0100

    feat: introduce automatic union of objects inferring during mapping
    When the mapper needs to map a source to a union of objects, it will try
    to guess which object it will map to, based on the needed arguments of
    the objects, and the values contained in the source.
    
    ```php
    final class UnionOfObjects
    {
        public readonly SomeFooObject|SomeBarObject $object;
    }
    
    final class SomeFooObject
    {
        public readonly string $foo;
    }
    
    final class SomeBarObject
    {
        public readonly string $bar;
    }
    
    // Will map to an instance of `SomeFooObject`
    (new \CuyZ\Valinor\MapperBuilder())
        ->mapper()
        ->map(UnionOfObjects::class, ['foo' => 'foo']);
    
    // Will map to an instance of `SomeBarObject`
    (new \CuyZ\Valinor\MapperBuilder())
        ->mapper()
        ->map(UnionOfObjects::class, ['bar' => 'bar']);
    ```

---
+++ src/Type/Resolver/Exception/CannotResolveObjectTypeFromUnion.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CuyZ\Valinor\Type\Resolver\Exception;
+
+use CuyZ\Valinor\Mapper\Tree\Message\Message;
+use CuyZ\Valinor\Type\Types\UnionType;
+use RuntimeException;
+
+final class CannotResolveObjectTypeFromUnion extends RuntimeException implements Message
+{
+    public function __construct(UnionType $unionType)
+    {
+        parent::__construct(
+            "Impossible to resolve the object type from the union `$unionType`.",
+            1641406600
+        );
+    }
+}
