042f59d95ce993377ef6a57929f0d2434ec0405a
Fred-Jan van der Eijken <fred-jan@vandereijken.nl>
2022-01-24 09:46:25 +0100

    Attributes now combine correctly with foreign attributes

--- src/Mapper/Object/Factory/AttributeObjectBuilderFactory.php
+++ src/Mapper/Object/Factory/AttributeObjectBuilderFactory.php
@@ -33,6 +33,6 @@ final class AttributeObjectBuilderFactory implements ObjectBuilderFactory
             throw new TooManyObjectBuilderFactoryAttributes($class, $attributes);
         }
 
-        return $attributes[0]->for($class, $source);
+        return \reset($attributes)->for($class, $source);
     }
 }
