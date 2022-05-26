99a4c73c8bf9ca8f6edeff943d6e3f172f197f6b
Filippo Tessarotto <zoeslam@gmail.com>
2022-05-26 08:53:07 +0200

    MappingError: expose the first error in Exception message

--- src/Mapper/MappingError.php
+++ src/Mapper/MappingError.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace CuyZ\Valinor\Mapper;
 
+use CuyZ\Valinor\Mapper\Tree\Message\MessagesFlattener;
 use CuyZ\Valinor\Mapper\Tree\Node;
 use RuntimeException;
 
@@ -16,8 +17,14 @@ final class MappingError extends RuntimeException
     {
         $this->node = $node;
 
+        $firstMessage = '';
+        foreach (new MessagesFlattener($node) as $message) {
+            $firstMessage = $message->withBody(" First error is:\nPath: {node_path}\nGiven value: {original_value}\nError: {original_message}")->__toString();
+            break;
+        }
+
         parent::__construct(
-            "Could not map type `{$node->type()}` with the given source.",
+            "Could not map type `{$node->type()}` with the given source.{$firstMessage}",
             1617193185
         );
     }
