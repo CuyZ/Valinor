e08310760a00c6c0019318c32fc9e0b4f9c4e470
Romain Canon <romain.hydrocanon@gmail.com>
2022-07-26 22:51:54 +0200

    misc: rework mapping error message

--- src/Mapper/MappingError.php
+++ src/Mapper/MappingError.php
@@ -6,6 +6,7 @@ namespace CuyZ\Valinor\Mapper;
 
 use CuyZ\Valinor\Mapper\Tree\Message\MessagesFlattener;
 use CuyZ\Valinor\Mapper\Tree\Node;
+use CuyZ\Valinor\Utility\ValueDumper;
 use RuntimeException;
 
 /** @api */
@@ -17,16 +18,21 @@ final class MappingError extends RuntimeException
     {
         $this->node = $node;
 
-        $firstMessage = '';
-        foreach (new MessagesFlattener($node) as $message) {
-            $firstMessage = $message->withBody(" First error is:\nPath: {node_path}\nGiven value: {original_value}\nError: {original_message}")->__toString();
-            break;
+        $source = ValueDumper::dump($node->sourceValue());
+
+        $errors = (new MessagesFlattener($node))->errors();
+        $errorsCount = count($errors);
+
+        $body = "Could not map type `{$node->type()}` with value $source. A total of $errorsCount errors were encountered.";
+
+        if ($errorsCount === 1) {
+            $body = $errors->getIterator()->current()
+                ->withParameter('root_type', $node->type())
+                ->withBody("Could not map type `{root_type}`. An error occurred at path {node_path}: {original_message}")
+                ->toString();
         }
 
-        parent::__construct(
-            "Could not map type `{$node->type()}` with the given source.{$firstMessage}",
-            1617193185
-        );
+        parent::__construct($body, 1617193185);
     }
 
     public function node(): Node
