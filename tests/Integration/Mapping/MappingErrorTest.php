17328d86dbedea0d1868a0ad1eaaf4fd8e8825bf
Romain Canon <romain.hydrocanon@gmail.com>
2022-07-27 08:16:57 +0200

    test: use exception expectation API for mapping error test

--- tests/Integration/Mapping/MappingErrorTest.php
+++ tests/Integration/Mapping/MappingErrorTest.php
@@ -10,42 +10,24 @@ use CuyZ\Valinor\Tests\Integration\IntegrationTest;
 
 final class MappingErrorTest extends IntegrationTest
 {
-    public function test_mapping_error_code_is_correct(): void
-    {
-        try {
-            (new MapperBuilder())->mapper()->map('string', ['foo']);
-
-            self::fail();
-        } catch (MappingError $exception) {
-            self::assertSame(1617193185, $exception->getCode());
-        }
-    }
-
     public function test_single_error_details_are_reported_in_exception_message(): void
     {
-        try {
-            (new MapperBuilder())->mapper()->map('string', ['foo']);
+        $this->expectException(MappingError::class);
+        $this->expectExceptionCode(1617193185);
+        $this->expectExceptionMessage("Could not map type `string`. An error occurred at path *root*: Value array{0: 'foo'} does not match type `string`.");
 
-            self::fail();
-        } catch (MappingError $exception) {
-            self::assertSame("Could not map type `string`. An error occurred at path *root*: Value array{0: 'foo'} does not match type `string`.", $exception->getMessage());
-        }
+        (new MapperBuilder())->mapper()->map('string', ['foo']);
     }
 
     public function test_several_errors_count_are_reported_in_exception_message(): void
     {
-        try {
-            (new MapperBuilder())->mapper()->map(
-                'array{foo: string, bar: int}',
-                ['foo' => 42, 'bar' => 'some string']
-            );
+        $this->expectException(MappingError::class);
+        $this->expectExceptionCode(1617193185);
+        $this->expectExceptionMessage("Could not map type `array{foo: string, bar: int}` with value array{foo: 42, bar: 'some string'}. A total of 2 errors were encountered.");
 
-            self::fail();
-        } catch (MappingError $exception) {
-            self::assertSame(
-                "Could not map type `array{foo: string, bar: int}` with value array{foo: 42, bar: 'some string'}. A total of 2 errors were encountered.",
-                $exception->getMessage()
-            );
-        }
+        (new MapperBuilder())->mapper()->map(
+            'array{foo: string, bar: int}',
+            ['foo' => 42, 'bar' => 'some string']
+        );
     }
 }
