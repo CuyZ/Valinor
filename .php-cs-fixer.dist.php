a92360897e8d73fe9509544e61f65afd6b09e5cd
Romain Canon <romain.hydrocanon@gmail.com>
2022-10-19 13:32:16 +0200

    feat: add support for PHP 8.2

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,7 +7,10 @@ $finder = PhpCsFixer\Finder::create()->in([
 ])
     ->notPath('Fixtures/FunctionWithGroupedImportStatements.php')
     ->notPath('Fixtures/FunctionWithSeveralImportStatementsInSameUseStatement.php')
-    ->notPath('Fixtures/TwoClassesInDifferentNamespaces.php');
+    ->notPath('Fixtures/TwoClassesInDifferentNamespaces.php')
+    // @PHP 8.2 remove when PHP-CS-Fixer fully supports PHP 8.2; also remove
+    // `@putenv PHP_CS_FIXER_IGNORE_ENV=1` inside `composer.json`
+    ->notPath('Fixture/Object/ObjectWithPropertyWithNativeDisjunctiveNormalFormType.php');
 
 if (PHP_VERSION_ID < 8_00_00) {
     $finder = $finder
---
+++ tests/Fixture/Object/ObjectWithPropertyWithNativeDisjunctiveNormalFormType.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CuyZ\Valinor\Tests\Fixture\Object;
+
+use Countable;
+use DateTime;
+use Iterator;
+
+// @PHP8.2 move to anonymous class
+final class ObjectWithPropertyWithNativeDisjunctiveNormalFormType
+{
+    public (Countable&Iterator)|(Countable&DateTime) $someProperty;
+}
