51efb1249c51a84cc2c0dadf72273d2102830a74
Romain Canon <romain.hydrocanon@gmail.com>
2022-10-21 12:04:37 +0200

    misc!: remove deprecated method used to bind a callback
    Constructors can now be registered using the method
    `\CuyZ\Valinor\MapperBuilder::registerConstructor()`.

--- tests/Unit/MapperBuilderTest.php
+++ tests/Unit/MapperBuilderTest.php
@@ -12,8 +12,6 @@ use DateTimeInterface;
 use PHPUnit\Framework\TestCase;
 use stdClass;
 
-use function sys_get_temp_dir;
-
 final class MapperBuilderTest extends TestCase
 {
     private MapperBuilder $mapperBuilder;
@@ -29,15 +27,14 @@ final class MapperBuilderTest extends TestCase
     {
         $builderA = $this->mapperBuilder;
         $builderB = $builderA->infer(DateTimeInterface::class, static fn () => DateTime::class);
-        $builderC = $builderA->bind(static fn (): DateTime => new DateTime());
-        $builderD = $builderA->registerConstructor(static fn (): stdClass => new stdClass());
-        $builderE = $builderA->alter(static fn (string $value): string => 'foo');
-        $builderF = $builderA->flexible();
-        $builderG = $builderA->enableFlexibleCasting();
-        $builderH = $builderA->allowSuperfluousKeys();
-        $builderI = $builderA->allowPermissiveTypes();
-        $builderJ = $builderA->filterExceptions(fn () => new FakeErrorMessage());
-        $builderK = $builderA->withCache(new FakeCache());
+        $builderC = $builderA->registerConstructor(static fn (): stdClass => new stdClass());
+        $builderD = $builderA->alter(static fn (string $value): string => 'foo');
+        $builderE = $builderA->flexible();
+        $builderF = $builderA->enableFlexibleCasting();
+        $builderG = $builderA->allowSuperfluousKeys();
+        $builderH = $builderA->allowPermissiveTypes();
+        $builderI = $builderA->filterExceptions(fn () => new FakeErrorMessage());
+        $builderJ = $builderA->withCache(new FakeCache());
 
         self::assertNotSame($builderA, $builderB);
         self::assertNotSame($builderA, $builderC);
@@ -48,7 +45,6 @@ final class MapperBuilderTest extends TestCase
         self::assertNotSame($builderA, $builderH);
         self::assertNotSame($builderA, $builderI);
         self::assertNotSame($builderA, $builderJ);
-        self::assertNotSame($builderA, $builderK);
     }
 
     public function test_mapper_instance_is_the_same(): void
