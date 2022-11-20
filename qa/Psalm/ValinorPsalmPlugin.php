a8950c5a77b8b573bf3b033f8c48d170e8dc4839
Romain Canon <romain.hydrocanon@gmail.com>
2022-11-20 23:22:14 +0100

    qa: enhance Psalm plugin registration for Valinor
    The plugin can now be registered like this in the `psalm.xml` file:
    
    ```xml
    <plugins>
        <pluginClass class="CuyZ\Valinor\QA\Psalm\ValinorPsalmPlugin"/>
    </plugins>
    ```

---
+++ qa/Psalm/ValinorPsalmPlugin.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CuyZ\Valinor\QA\Psalm;
+
+use CuyZ\Valinor\QA\Psalm\Plugin\TreeMapperPsalmPlugin;
+use Psalm\Plugin\PluginEntryPointInterface;
+use Psalm\Plugin\RegistrationInterface;
+use SimpleXMLElement;
+
+class ValinorPsalmPlugin implements PluginEntryPointInterface
+{
+    public function __invoke(RegistrationInterface $api, SimpleXMLElement $config = null): void
+    {
+        require_once __DIR__ . '/Plugin/TreeMapperPsalmPlugin.php';
+
+        $api->registerHooksFromClass(TreeMapperPsalmPlugin::class);
+    }
+}
