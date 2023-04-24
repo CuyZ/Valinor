861c3b9da7f5c762db11da2ba9bbfc1cbdeaace8
Tim Düsterhus <duesterhus@woltlab.com>
2023-04-24 14:57:11 +0200

    misc: catch `json_encode` exception to help identifying parsing errors
    This brings two improvements:
    
    - The reason for the JSON parsing failure is visible in the stack
      trace / `$previous` exception, allowing the developer to determine
      whether the parsing failure was caused by a syntax error, invalid
      UTF-8 sequence or something else.
    - The JSON string null (which decodes to null) is no longer erroneously
      rejected as invalid. Instead it is rejected, because it is not
      iterable, slightly improving the error reporting.

--- src/Mapper/Source/Exception/InvalidJson.php
+++ src/Mapper/Source/Exception/InvalidJson.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace CuyZ\Valinor\Mapper\Source\Exception;
 
+use JsonException;
 use RuntimeException;
 
 /** @internal */
 final class InvalidJson extends RuntimeException implements InvalidSource
 {
-    public function __construct(private string $source)
+    public function __construct(private string $source, ?JsonException $previous = null)
     {
         parent::__construct(
             'Invalid JSON source.',
-            1566307185
+            1566307185,
+            $previous
         );
     }
 
