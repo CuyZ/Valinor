29da9acd97db6f43bfc7db79b83755cca16734bd
Tim Düsterhus <duesterhus@woltlab.com>
2023-08-22 20:08:37 +0200

    fix: handle invalid Intl formats with `intl.use_exceptions=1`
    If `intl.use_exceptions=1`, ext/intl will throw an `\IntlException` that
    was not yet handled and caused `StringFormatter::format()` to violate
    its contract to throw `StringFormatterError` in case of errors.

--- src/Utility/String/StringFormatterError.php
+++ src/Utility/String/StringFormatterError.php
@@ -9,11 +9,11 @@ use RuntimeException;
 /** @internal */
 final class StringFormatterError extends RuntimeException
 {
-    public function __construct(string $body, string $message = '')
+    public function __construct(string $body, string $message = '', ?\Throwable $previous = null)
     {
         if ($message !== '') {
             $message = ": $message";
         }
-        parent::__construct("Message formatter error using `$body`$message.", 1652901203);
+        parent::__construct("Message formatter error using `$body`$message.", 1652901203, $previous);
     }
 }
