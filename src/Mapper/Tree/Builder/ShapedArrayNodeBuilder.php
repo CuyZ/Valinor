e8fc445e4721fe09a7a790a1da4fca3b33aab86a
Daniil Gentili <daniil@daniil.it>
2023-10-02 20:36:59 +0200

    Fix phpstan SA

--- src/Mapper/Tree/Builder/ShapedArrayNodeBuilder.php
+++ src/Mapper/Tree/Builder/ShapedArrayNodeBuilder.php
@@ -75,8 +75,8 @@ final class ShapedArrayNodeBuilder implements NodeBuilder
 
         if (!$type->sealed()) {
             foreach ($value as $k => $v) {
-                if (!array_key_exists($key, $children)) {
-                    $children[$key] = TreeNode::leaf(Shell::root(new MixedType(), $v), $v);
+                if (!array_key_exists($k, $children)) {
+                    $children[$k] = TreeNode::leaf(Shell::root(new MixedType(), $v), $v);
                 }
             }
         }
