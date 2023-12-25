4f4af0ac1b20c7b59bc913a7dfd808dff718b6e2
AndrolGenhald <AndrolGenhald@users.noreply.github.com>
2023-12-25 15:50:08 -0600

    fix: allow mapping union of scalars and classes
    Co-authored-by: vdauchy <26772554+vdauchy@users.noreply.github.com>

---
+++ tests/Integration/Mapping/UnionMappingTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CuyZ\Valinor\Tests\Integration\Mapping;
+
+use CuyZ\Valinor\Mapper\MappingError;
+use CuyZ\Valinor\MapperBuilder;
+use CuyZ\Valinor\Tests\Integration\IntegrationTest;
+use CuyZ\Valinor\Tests\Integration\Mapping\Fixture\City;
+use CuyZ\Valinor\Tests\Integration\Mapping\Fixture\SimpleObject;
+
+final class UnionMappingTest extends IntegrationTest
+{
+    public function test_union_with_int_or_object(): void
+    {
+        try {
+            $array = (new MapperBuilder())->mapper()->map("list<int|" . SimpleObject::class . ">", [123, "foo"]);
+        } catch (MappingError $error) {
+            $this->mappingFail($error);
+        }
+
+        self::assertSame(123, $array[0]);
+        self::assertInstanceOf(SimpleObject::class, $array[1]);
+    }
+
+    public function test_union_with_string_or_object_prioritizes_string(): void
+    {
+        try {
+            $array = (new MapperBuilder())
+                ->mapper()
+                ->map("list<string|" . SimpleObject::class . ">", ["foo", "bar", "baz"]);
+        } catch (MappingError $error) {
+            $this->mappingFail($error);
+        }
+
+        self::assertSame(["foo", "bar", "baz"], $array);
+    }
+
+    public function test_union_with_string_literal_or_object_prioritizes_string_literal(): void
+    {
+        try {
+            $array = (new MapperBuilder())
+                ->mapper()
+                ->map("list<'foo'|" . SimpleObject::class . "|'bar'>", ["foo", "bar", "baz"]);
+        } catch (MappingError $error) {
+            $this->mappingFail($error);
+        }
+
+        self::assertSame("foo", $array[0]);
+        self::assertSame("bar", $array[1]);
+        self::assertInstanceOf(SimpleObject::class, $array[2]);
+    }
+
+    public function test_union_of_objects(): void
+    {
+        try {
+            $array = (new MapperBuilder())
+                ->mapper()
+                ->map(
+                    "list<" . SimpleObject::class . "|" . City::class . ">",
+                    ["foo", ["name" => "foobar", "timeZone" => "UTC"], "baz"],
+                );
+        } catch (MappingError $error) {
+            $this->mappingFail($error);
+        }
+
+        self::assertInstanceOf(SimpleObject::class, $array[0]);
+        self::assertInstanceOf(City::class, $array[1]);
+        self::assertInstanceOf(SimpleObject::class, $array[2]);
+    }
+}
