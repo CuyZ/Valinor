b1017ce55729f0698c7629d57a3d3a30c0f9bff3
Robert Chettleburgh <robchett@gmail.com>
2024-07-19 15:52:43 +0100

    feat: add support for `value-of<BackedEnum>` type
    This type can be used as follows:
    
    ```php
    enum Suit: string
    {
        case Hearts = 'H';
        case Diamonds = 'D';
        case Clubs = 'C';
        case Spades = 'S';
    }
    
    $suit = (new \CuyZ\Valinor\MapperBuilder())
        ->mapper()
        ->map(Suit::class, 'D');
    
    // $suit === Suit::Diamonds
    ```

---
+++ src/Type/Parser/Exception/Magic/ValueOfClosingBracketMissing.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CuyZ\Valinor\Type\Parser\Exception\Magic;
+
+use CuyZ\Valinor\Type\Parser\Exception\InvalidType;
+use CuyZ\Valinor\Type\Type;
+use RuntimeException;
+
+/** @internal */
+final class ValueOfClosingBracketMissing extends RuntimeException implements InvalidType
+{
+    public function __construct(Type $type)
+    {
+        parent::__construct(
+            "The closing bracket is missing for `value-of<{$type->toString()}>`.",
+            1717702289
+        );
+    }
+}
---
+++ src/Type/Parser/Exception/Magic/ValueOfOpeningBracketMissing.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CuyZ\Valinor\Type\Parser\Exception\Magic;
+
+use CuyZ\Valinor\Type\Parser\Exception\InvalidType;
+use RuntimeException;
+
+/** @internal */
+final class ValueOfOpeningBracketMissing extends RuntimeException implements InvalidType
+{
+    public function __construct()
+    {
+        parent::__construct(
+            "The opening bracket is missing for `value-of<...>`.",
+            1717702268
+        );
+    }
+}
