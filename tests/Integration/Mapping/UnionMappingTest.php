CuyZ/Valinor

commit 0479532fbc96fca35dcbfb4c1f5a9ef63e7625c5

misc: move "float type accepting integer value" logic in `Shell`

src/Mapper/Tree/Shell.php
@@ -7,6 +7,7 @@ namespace CuyZ\Valinor\Mapper\Tree;
 use CuyZ\Valinor\Definition\Attributes;
 use CuyZ\Valinor\Library\Settings;
 use CuyZ\Valinor\Mapper\Tree\Exception\UnresolvableShellType;
+use CuyZ\Valinor\Type\FloatType;
 use CuyZ\Valinor\Type\Type;
 use CuyZ\Valinor\Type\Types\UnresolvableType;
 
@@ -87,6 +88,14 @@ final class Shell
 
     public function withValue(mixed $value): self
     {
+        // When the value is an integer and the type is a float, the value is
+        // cast to float, to follow the rule of PHP regarding acceptance of an
+        // integer value in a float type. Note that PHPStan/Psalm analysis
+        // applies the same rule.
+        if ($this->type instanceof FloatType && is_int($value)) {
+            $value = (float)$value;
+        }
+
         $clone = clone $this;
         $clone->hasValue = true;
         $clone->value = $value;

src/Mapper/Tree/Builder/TreeNode.php
@@ -9,7 +9,6 @@ use CuyZ\Valinor\Mapper\Tree\Exception\UnexpectedKeysInSource;
 use CuyZ\Valinor\Mapper\Tree\Message\Message;
 use CuyZ\Valinor\Mapper\Tree\Node;
 use CuyZ\Valinor\Mapper\Tree\Shell;
-use CuyZ\Valinor\Type\FloatType;
 use CuyZ\Valinor\Type\Type;
 use Throwable;
 
@@ -35,14 +34,6 @@ final class TreeNode
 
     private function __construct(Shell $shell, mixed $value)
     {
-        // When the value is an integer and the type is a float, the value needs
-        // to be cast to float — this special case needs to be handled in case a
-        // node is not a *native* PHP float type (for instance a class property
-        // with a `@var float` annotation).
-        if ($shell->type() instanceof FloatType && is_int($value)) {
-            $value = (float)$value;
-        }
-
         $this->shell = $shell;
         $this->value = $value;
     }

src/Type/Types/NativeFloatType.php
@@ -12,7 +12,6 @@ use CuyZ\Valinor\Utility\IsSingleton;
 
 use function assert;
 use function is_float;
-use function is_integer;
 use function is_numeric;
 
 /** @internal */
@@ -22,7 +21,7 @@ final class NativeFloatType implements FloatType
 
     public function accepts(mixed $value): bool
     {
-        return is_float($value) || is_integer($value);
+        return is_float($value);
     }
 
     public function matches(Type $other): bool

tests/Unit/Type/Types/NativeFloatTypeTest.php
@@ -30,13 +30,13 @@ final class NativeFloatTypeTest extends TestCase
     public function test_accepts_correct_values(): void
     {
         self::assertTrue($this->floatType->accepts(42.1337));
-        self::assertTrue($this->floatType->accepts(404));
     }
 
     public function test_does_not_accept_incorrect_values(): void
     {
         self::assertFalse($this->floatType->accepts(null));
         self::assertFalse($this->floatType->accepts('Schwifty!'));
+        self::assertFalse($this->floatType->accepts(404));
         self::assertFalse($this->floatType->accepts(['foo' => 'bar']));
         self::assertFalse($this->floatType->accepts(false));
         self::assertFalse($this->floatType->accepts(new stdClass()));

commit 9dd83fc7e45365f654e90efd0106a4acd9ed89a8

Fix int and float compatibility in union types
This was broken in commit
https://github.com/CuyZ/Valinor/commit/0479532fbc96fca35dcbfb4c1f5a9ef63e7625c5
as withType may be called in union type context.
First calling withValue() to set an int value and then updating the type
with withType() does not trigger the float cast, in turn causing a
mapping error.

Developed by: Social Deal (@socialdeal)

src/Mapper/Tree/Shell.php
@@ -80,6 +80,7 @@ final class Shell
     {
         $clone = clone $this;
         $clone->type = $newType;
+        $clone->value = self::castCompatibleValue($newType, $this->value);
 
         return $clone;
     }
@@ -91,17 +92,9 @@ final class Shell
 
     public function withValue(mixed $value): self
     {
-        // When the value is an integer and the type is a float, the value is
-        // cast to float, to follow the rule of PHP regarding acceptance of an
-        // integer value in a float type. Note that PHPStan/Psalm analysis
-        // applies the same rule.
-        if ($this->type instanceof FloatType && is_int($value)) {
-            $value = (float)$value;
-        }
-
         $clone = clone $this;
         $clone->hasValue = true;
-        $clone->value = $value;
+        $clone->value = self::castCompatibleValue($clone->type, $value);
 
         return $clone;
     }
@@ -172,5 +165,18 @@ final class Shell
         }
 
         return implode('.', $path);
+    }
+
+    private static function castCompatibleValue(Type $type, mixed $value): mixed
+    {
+        // When the value is an integer and the type is a float, the value is
+        // cast to float, to follow the rule of PHP regarding acceptance of an
+        // integer value in a float type. Note that PHPStan/Psalm analysis
+        // applies the same rule.
+        if ($type instanceof FloatType && is_int($value)) {
+            return (float)$value;
+        }
+
+        return $value;
     }
 }

tests/Integration/Mapping/UnionMappingTest.php
@@ -64,6 +64,15 @@ final class UnionMappingTest extends IntegrationTestCase
             'assertion' => fn (mixed $result) => self::assertNull($result),
         ];
 
+        yield 'nullable float with integer value' => [
+            'type' => 'float|null',
+            'source' => 42,
+            'assertion' => function (mixed $result) {
+                self::assertIsFloat($result);
+                self::assertEquals(42.0, $result);
+            },
+        ];
+
         yield 'string or list of string, with string' => [
             'type' => 'string|list<string>',
             'source' => 'foo',