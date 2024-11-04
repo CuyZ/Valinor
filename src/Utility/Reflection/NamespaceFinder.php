7a0fc21db0413fbec852c65d641c0e08372a2869
Romain Canon <romain.hydrocanon@gmail.com>
2024-11-04 09:52:53 +0100

    fix: handle namespace for Closure without class scope

---
+++ src/Utility/Reflection/NamespaceFinder.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CuyZ\Valinor\Utility\Reflection;
+
+use PhpToken;
+
+/** @internal */
+final class NamespaceFinder
+{
+    public function findNamespace(string $content): ?string
+    {
+        $tokens = PhpToken::tokenize($content);
+        $tokensCount = count($tokens);
+        /* @infection-ignore-all Unneeded because of the nature of namespace-related token */
+        $pointer = $tokensCount - 1;
+
+        while (! $tokens[$pointer]->is(T_NAMESPACE)) {
+            /* @infection-ignore-all Unneeded because of the nature of namespace-related token */
+            if ($pointer === 0) {
+                return null;
+            }
+
+            $pointer--;
+        }
+
+        while (! $tokens[$pointer]->is([T_NAME_QUALIFIED, T_STRING])) {
+            $pointer++;
+        }
+
+        return (string)$tokens[$pointer];
+    }
+}
