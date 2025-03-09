d696f8b967b2cbaf91124555ac98247775f8f5a2
Romain Canon <romain.hydrocanon@gmail.com>
2025-03-09 20:14:10 +0100

    misc: add doc to property

--- src/Normalizer/Transformer/Compiler/Definition/TransformerDefinition.php
+++ src/Normalizer/Transformer/Compiler/Definition/TransformerDefinition.php
@@ -12,6 +12,32 @@ use CuyZ\Valinor\Type\Type;
 /** @internal */
 final class TransformerDefinition
 {
+    /**
+     * This flag is used to indicate that we are sure what the type of this
+     * definition is. This is important to ensure that the normalizer will act
+     * correctly, even if the value of a property does not respect the expected
+     * type.
+     *
+     * For instance, in the following case we know that the value is a string:
+     *
+     * ```php
+     * final class SomeClass
+     * {
+     *     public string $value;
+     * }
+     * ```
+     *
+     * However, in the following case, we cannot guarantee the type of the
+     * value, as it may contain any value.
+     *
+     * ```php
+     *  final class SomeClass
+     *  {
+     *      /** @var string *\
+     *      public $value;
+     *  }
+     *  ```
+     */
     private bool $isSure = false;
 
     public function __construct(
