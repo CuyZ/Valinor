2d3f2b286af00adfb5a1d186fbfe45f179cdda1f
Romain Canon <romain.hydrocanon@gmail.com>
2025-04-16 21:10:30 +0200

    misc: remove `Throwable` inheritance from `ErrorMessage`
    The error messages being exceptions do not add any value when using the
    messages in a `MappingError`. Therefore, the backtrace creation is a
    heavy, unnecessary, operation.
    
    All internal messages have been changed to reflect this.
    
    Throwing custom exceptions during object creations still works.

--- src/Mapper/Tree/Exception/InvalidArrayKey.php
+++ src/Mapper/Tree/Exception/InvalidArrayKey.php
@@ -5,17 +5,18 @@ declare(strict_types=1);
 namespace CuyZ\Valinor\Mapper\Tree\Exception;
 
 use CuyZ\Valinor\Mapper\Tree\Message\ErrorMessage;
+use CuyZ\Valinor\Mapper\Tree\Message\HasCode;
 use CuyZ\Valinor\Mapper\Tree\Message\HasParameters;
 use CuyZ\Valinor\Type\Types\ArrayKeyType;
-use CuyZ\Valinor\Utility\String\StringFormatter;
 use CuyZ\Valinor\Utility\TypeHelper;
 use CuyZ\Valinor\Utility\ValueDumper;
-use RuntimeException;
 
 /** @internal */
-final class InvalidTraversableKey extends RuntimeException implements ErrorMessage, HasParameters
+final class InvalidArrayKey implements ErrorMessage, HasCode, HasParameters
 {
     private string $body = 'Key {key} does not match type {expected_type}.';
+
+    private string $code = '1630946163';
 
     /** @var array<string, string> */
     private array $parameters;
@@ -26,13 +27,16 @@ final class InvalidTraversableKey extends RuntimeException implements ErrorMessa
             'key' => ValueDumper::dump($key),
             'expected_type' => TypeHelper::dump($type),
         ];
-
-        parent::__construct(StringFormatter::for($this), 1630946163);
     }
 
     public function body(): string
     {
         return $this->body;
+    }
+
+    public function code(): string
+    {
+        return $this->code;
     }
 
     public function parameters(): array
