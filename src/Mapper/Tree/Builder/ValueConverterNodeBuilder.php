46c823e9fe85bc2771b587eaeecbab11c5994f5b
Romain Canon <romain.hydrocanon@gmail.com>
2025-05-26 13:49:28 +0200

    feat: introduce mapper converters to apply custom logic during mapping
    A mapper converter allows users to hook into the mapping process and
    apply custom logic to the input, by defining a callable signature that
    properly describes when it should be called:
    
    - A first argument with a type matching the expected input being mapped
    - A return type representing the targeted mapped type
    
    These two types are enough for the library to know when to call the
    converter and can contain advanced type annotations for more specific
    use cases.
    
    Below is a basic example of a converter that converts string inputs to
    uppercase:
    
    ```php
    (new \CuyZ\Valinor\MapperBuilder())
        ->registerConverter(
            fn (string $value): string => strtoupper($value)
        )
        ->mapper()
        ->map('string', 'hello world'); // 'HELLO WORLD'
    ```
    
    Converters can be chained, allowing multiple transformations to be
    applied to a value. A second `callable` parameter can be declared,
    allowing the current converter to call the next one in the chain.
    
    A priority can be given to a converter to control the order in which
    converters are applied. The higher the priority, the earlier the
    converter will be executed. The default priority is 0.
    
    ```php
    (new \CuyZ\Valinor\MapperBuilder())
        ->registerConverter(
            function(string $value, callable $next): string {
                return $next(strtoupper($value));
            }
        )
        ->registerConverter(
            function(string $value, callable $next): string {
                return $next($value . '!');
            },
            priority: -10,
        )
        ->registerConverter(
            function(string $value, callable $next): string {
                return $next($value . '?');
            },
            priority: 10,
        )
        ->mapper()
        ->map('string', 'hello world'); // 'HELLO WORLD?!'
    ```

---
+++ src/Mapper/Tree/Builder/ValueConverterNodeBuilder.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CuyZ\Valinor\Mapper\Tree\Builder;
+
+use CuyZ\Valinor\Definition\FunctionObject;
+use CuyZ\Valinor\Definition\FunctionsContainer;
+use CuyZ\Valinor\Mapper\Tree\Exception\InvalidNodeDuringValueConversion;
+use CuyZ\Valinor\Mapper\Tree\Exception\ValueConverterHasNoArgument;
+use CuyZ\Valinor\Mapper\Tree\Shell;
+
+use function array_shift;
+use function is_float;
+use function is_nan;
+
+/** @internal */
+final class ValueConverterNodeBuilder implements NodeBuilder
+{
+    public function __construct(
+        private NodeBuilder $delegate,
+        private FunctionsContainer $functions,
+    ) {}
+
+    public function build(Shell $shell, RootNodeBuilder $rootBuilder): Node
+    {
+        try {
+            $result = $this->unstack($this->functions->toArray(), $shell, $rootBuilder);
+
+            return Node::new($result);
+        } catch (InvalidNodeDuringValueConversion $exception) {
+            return $exception->node;
+        }
+    }
+
+    /**
+     * @param array<FunctionObject> $stack
+     */
+    private function unstack(array $stack, Shell $shell, RootNodeBuilder $rootBuilder): mixed
+    {
+        while ($current = array_shift($stack)) {
+            if ($current->definition->parameters->count() === 0) {
+                throw new ValueConverterHasNoArgument($current->definition);
+            }
+
+            if (! $shell->type()->matches($current->definition->returnType)) {
+                continue;
+            }
+
+            if (! $current->definition->parameters->at(0)->type->accepts($shell->value())) {
+                continue;
+            }
+
+            $arguments = [$shell->value()];
+
+            if ($current->definition->parameters->count() >= 2) {
+                $arguments[] = function (mixed $value = NAN) use ($stack, $shell, $rootBuilder) {
+                    if (! is_float($value) || ! is_nan($value)) {
+                        $shell = $shell->withValue($value);
+                    }
+
+                    return $this->unstack($stack, $shell, $rootBuilder);
+                };
+            }
+
+            return ($current->callback)(...$arguments);
+        }
+
+        $node = $this->delegate->build($shell, $rootBuilder);
+
+        if (! $node->isValid()) {
+            throw new InvalidNodeDuringValueConversion($node);
+        }
+
+        return $node->value();
+    }
+}
---
+++ tests/Integration/Mapping/ValueConverterMappingTest.php
@@ -0,0 +1,214 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CuyZ\Valinor\Tests\Integration\Mapping;
+
+use CuyZ\Valinor\Mapper\MappingError;
+use CuyZ\Valinor\Mapper\Tree\Exception\ValueConverterHasNoArgument;
+use CuyZ\Valinor\Tests\Integration\IntegrationTestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+final class ValueConverterMappingTest extends IntegrationTestCase
+{
+    /**
+     * @param array<int, list<callable>> $convertersByPriority
+     */
+    #[DataProvider('value_is_converted_properly_data_provider')]
+    public function test_value_is_converted_properly(string $type, mixed $value, mixed $expectedResult, array $convertersByPriority): void
+    {
+        try {
+            $builder = $this->mapperBuilder();
+
+            foreach ($convertersByPriority as $priority => $converters) {
+                foreach ($converters as $converter) {
+                    $builder = $builder->registerConverter($converter, $priority);
+                }
+            }
+
+            $result = $builder
+                ->mapper()
+                ->map($type, $value);
+
+            self::assertSame($expectedResult, $result);
+        } catch (MappingError $error) {
+            $this->mappingFail($error);
+        }
+    }
+
+    public static function value_is_converted_properly_data_provider(): iterable
+    {
+        yield 'string converted to uppercase' => [
+            'type' => 'string',
+            'value' => 'foo',
+            'expectedResult' => 'FOO',
+            'convertersByPriority' => [
+                [
+                    strtoupper(...),
+                ],
+            ],
+        ];
+
+        yield 'string with converter that calls next without value' => [
+            'type' => 'string',
+            'value' => 'foo',
+            'expectedResult' => 'foo!',
+            'convertersByPriority' => [
+                [
+                    fn (string $value, callable $next) => $next() . '!', // @phpstan-ignore binaryOp.invalid (we cannot set closure parameters / see https://github.com/phpstan/phpstan/issues/3770)
+                ],
+            ],
+        ];
+
+        yield 'string with prioritized converters' => [
+            'type' => 'string',
+            'value' => 'foo',
+            'expectedResult' => 'foo?!',
+            'convertersByPriority' => [
+                10 => [
+                    fn (string $value, callable $next): string => $next($value . '!'), // @phpstan-ignore return.type (we cannot set closure parameters / see https://github.com/phpstan/phpstan/issues/3770)
+                ],
+                50 => [
+                    fn (string $value, callable $next): string => $next($value . '?'), // @phpstan-ignore return.type (we cannot set closure parameters / see https://github.com/phpstan/phpstan/issues/3770)
+                ],
+            ],
+        ];
+
+        yield 'string with ignored converters' => [
+            'type' => 'string',
+            'value' => 'foo',
+            'expectedResult' => 'foo!',
+            'convertersByPriority' => [
+                [
+                    fn (int $value, callable $next): string => $next($value) . '?', // @phpstan-ignore binaryOp.invalid (we cannot set closure parameters / see https://github.com/phpstan/phpstan/issues/3770)
+                    fn (int $value, callable $next): int => $next($value + 1), // @phpstan-ignore return.type (we cannot set closure parameters / see https://github.com/phpstan/phpstan/issues/3770)
+                    fn (string $value, callable $next): string => $next($value . '!'), // @phpstan-ignore return.type (we cannot set closure parameters / see https://github.com/phpstan/phpstan/issues/3770)
+                    fn (int $value, callable $next): int => $next($value + 2), // @phpstan-ignore return.type (we cannot set closure parameters / see https://github.com/phpstan/phpstan/issues/3770)
+                    fn (int $value, callable $next): string => $next($value) . '#', // @phpstan-ignore binaryOp.invalid (we cannot set closure parameters / see https://github.com/phpstan/phpstan/issues/3770)
+                ],
+            ],
+        ];
+
+        yield 'string with converter that does not call next' => [
+            'type' => 'string',
+            'value' => 'foo',
+            'expectedResult' => 'foo!',
+            'convertersByPriority' => [
+                [
+                    fn (string $value): string => $value . '!',
+                    fn (string $value, callable $next): string => $next($value . '?'), // @phpstan-ignore return.type (we cannot set closure parameters / see https://github.com/phpstan/phpstan/issues/3770)
+                ],
+            ],
+        ];
+    }
+
+    public function test_converter_with_no_priority_has_priority_0_by_default(): void
+    {
+        $result = $this->mapperBuilder()
+            ->registerConverter(fn (string $value, callable $next) => $next($value . '!'), -1)
+            ->registerConverter(fn (string $value, callable $next) => $next($value . '?'))
+            ->registerConverter(fn (string $value, callable $next) => $next($value . '#'), 1)
+            ->mapper()
+            ->map('string', 'foo');
+
+        self::assertSame('foo#?!', $result);
+    }
+
+    public function test_converter_is_stopped_if_mapping_error_occurs(): void
+    {
+        try {
+            $this->mapperBuilder()
+                ->registerConverter(fn (string $value, callable $next) => $next(42))
+                ->mapper()
+                ->map('string', 'foo');
+        } catch (MappingError $exception) {
+            self::assertMappingErrors($exception, [
+                '*root*' => '[invalid_string] Value 42 is not a valid string.',
+            ]);
+        }
+    }
+
+    public function test_converter_with_no_argument_throws_exception(): void
+    {
+        $this->expectException(ValueConverterHasNoArgument::class);
+        $this->expectExceptionCode(1746449489);
+        $this->expectExceptionMessageMatches('/The value converter `.*` has no argument to convert the value to, a typed argument is required\./');
+
+        $this->mapperBuilder()
+            ->registerConverter(fn () => 'bar')
+            ->mapper()
+            ->map('string', 'foo');
+    }
+
+    public function test_can_convert_snake_case_to_camel_case_for_object(): void
+    {
+        $class = new class () {
+            public string $someValue;
+
+            /** @var array<string> */
+            public array $someOtherValue;
+        };
+
+        try {
+            $result = $this->mapperBuilder()
+                // @phpstan-ignore return.type (we cannot set closure parameters / see https://github.com/phpstan/phpstan/issues/3770)
+                ->registerConverter(fn (array $values, callable $next): object => $next(array_combine(
+                    array_map(
+                        fn ($key) => lcfirst(str_replace([' ', '_', '-'], '', ucwords($key, ' _-'))),
+                        array_keys($values),
+                    ),
+                    $values,
+                )))
+                ->mapper()
+                ->map($class::class, [
+                    'some_value' => 'foo',
+                    'some_other_value' => [
+                        'bar' => 'bar',
+                        'baz' => 'baz',
+                    ],
+                ]);
+        } catch (MappingError $error) {
+            $this->mappingFail($error);
+        }
+
+        self::assertSame('foo', $result->someValue);
+        self::assertSame(['bar' => 'bar', 'baz' => 'baz'], $result->someOtherValue);
+    }
+
+    public function test_can_rename_keys_for_object(): void
+    {
+        $class = new class () {
+            public string $someValue;
+
+            public int $someOtherValue;
+        };
+
+        $renameKeys = fn (array $values, array $keyReplacements) => array_combine(
+            // @phpstan-ignore argument.type (we cannot set closure parameters / see https://github.com/phpstan/phpstan/issues/3770)
+            array_map(
+                fn ($key) => $keyReplacements[$key] ?? $key,
+                array_keys($values),
+            ),
+            $values,
+        );
+
+        try {
+            $result = $this->mapperBuilder()
+                // @phpstan-ignore return.type (we cannot set closure parameters / see https://github.com/phpstan/phpstan/issues/3770)
+                ->registerConverter(fn (array $values, callable $next): object => $next($renameKeys($values, [
+                    'aValue' => 'someValue',
+                    'anotherValue' => 'someOtherValue',
+                ])))
+                ->mapper()
+                ->map($class::class, [
+                    'aValue' => 'foo',
+                    'anotherValue' => 42,
+                ]);
+        } catch (MappingError $error) {
+            $this->mappingFail($error);
+        }
+
+        self::assertSame('foo', $result->someValue);
+        self::assertSame(42, $result->someOtherValue);
+    }
+}
