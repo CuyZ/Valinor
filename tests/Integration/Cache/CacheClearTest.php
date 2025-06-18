60a39f645221b97b3d66f3fb8fbdbb47f9218e2a
Romain Canon <romain.hydrocanon@gmail.com>
2025-06-18 16:48:21 +0200

    feat: allow `MapperBuilder` and `NormalizerBuilder` to clear cache

---
+++ tests/Integration/Cache/CacheClearTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CuyZ\Valinor\Tests\Integration\Cache;
+
+use CuyZ\Valinor\Normalizer\Format;
+use CuyZ\Valinor\Tests\Fake\Cache\FakeCache;
+use CuyZ\Valinor\Tests\Integration\IntegrationTestCase;
+
+final class CacheClearTest extends IntegrationTestCase
+{
+    public function test_call_mapper_builder_clear_cache_clears_cache(): void
+    {
+        $cache = new FakeCache();
+
+        $mapperBuilder = $this->mapperBuilder()->withCache($cache);
+
+        $mapperBuilder->warmup('array<stdClass>');
+
+        self::assertGreaterThan(0, $cache->countEntries());
+
+        $mapperBuilder->clearCache();
+
+        self::assertSame(0, $cache->countEntries());
+    }
+
+    public function test_call_normalizer_builder_clear_cache_clears_cache(): void
+    {
+        $cache = new FakeCache();
+
+        $mapperBuilder = $this->normalizerBuilder()->withCache($cache);
+
+        $mapperBuilder->normalizer(Format::array())->normalize('foo');
+
+        self::assertGreaterThan(0, $cache->countEntries());
+
+        $mapperBuilder->clearCache();
+
+        self::assertSame(0, $cache->countEntries());
+    }
+}
