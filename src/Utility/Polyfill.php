5407419b1a02806f9dd35a1ece025aece878220d
Romain Canon <romain.hydrocanon@gmail.com>
2025-07-16 12:59:39 +0200

    fix: use polyfill for `array_find`

--- src/Utility/Polyfill.php
+++ src/Utility/Polyfill.php
@@ -22,4 +22,19 @@ final class Polyfill
 
         return true;
     }
+    /**
+     * PHP8.4 use native function `array_find` instead.
+     *
+     * @param array<mixed> $array
+     */
+    public static function array_find(array $array, callable $callback): mixed
+    {
+        foreach ($array as $key => $value) {
+            if ($callback($value, $key)) {
+                return $value;
+            }
+        }
+
+        return null;
+    }
 }
