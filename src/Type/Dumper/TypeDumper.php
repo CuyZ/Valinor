7231082b7c1f81c3a12f3a7b44f7b97964f863c5
Romain Canon <ext-r.canon@vattenfall.fr>
2025-09-18 14:24:56 +0200

    fix: truncate everything when the first level is too long

--- src/Type/Dumper/TypeDumper.php
+++ src/Type/Dumper/TypeDumper.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace CuyZ\Valinor\Type\Dumper;
 
 use CuyZ\Valinor\Definition\Repository\ClassDefinitionRepository;
@@ -12,11 +14,11 @@ use CuyZ\Valinor\Type\Types\EnumType;
 use CuyZ\Valinor\Utility\TypeHelper;
 
 use function array_map;
+use function count;
+use function implode;
+use function sprintf;
 use function strlen;
 use function usort;
-use function sprintf;
-use function implode;
-use function count;
 
 /** @internal */
 final class TypeDumper
@@ -25,33 +27,43 @@ final class TypeDumper
 
     public function __construct(
         private readonly ClassDefinitionRepository $classDefinitionRepository,
-        private readonly ObjectBuilderFactory $objectBuilderFactory
+        private readonly ObjectBuilderFactory $objectBuilderFactory,
     ) {}
 
     public function dump(Type $type): string
     {
-        return $this->doDump(type: $type, length: 0, weight: 0);
-    }
+        $dump = $this->doDump(type: $type, weight: 0, truncate: false);
 
-    private function doDump(Type $type, int $length, int $weight): string
-    {
-        if ($type instanceof EnumType) {
-            return $type->readableSignature();
-        } elseif ($type instanceof ObjectType) {
-            return $this->getStringTypeFromObject($type, $length, $weight);
+        // If the dump is too long, we re-dump everything but the sub-objects
+        // are being truncated to lower the dump length.
+        if (strlen($dump) > self::MAX_LENGTH) {
+            return $this->doDump(type: $type, weight: 0, truncate: true);
         }
 
-        return $type->toString();
+        return $dump;
     }
 
-    private function getStringTypeFromObject(ObjectType $type, int $length, int $weight): string
+    private function doDump(Type $type, int $weight, bool $truncate): string
     {
+        return match (true) {
+            $type instanceof EnumType => $type->readableSignature(),
+            $type instanceof ObjectType => $this->getStringTypeFromObject($type, $weight, $truncate),
+            default => $type->toString(),
+        };
+    }
+
+    private function getStringTypeFromObject(ObjectType $type, int $weight, bool $truncate): string
+    {
+        if ($truncate && $weight > 0) {
+            return 'array{…}';
+        }
+
         $class = $this->classDefinitionRepository->for($type);
         $objectBuilders = $this->objectBuilderFactory->for($class);
 
         $textArray = array_map(
-            fn (ObjectBuilder $builder) => $this->formatArguments($builder->describeArguments(), $length, $weight),
-            $objectBuilders
+            fn (ObjectBuilder $builder) => $this->formatArguments($builder->describeArguments(), $weight, $truncate),
+            $objectBuilders,
         );
 
         usort($textArray, static fn ($a, $b) => $a['weight'] <=> $b['weight']);
@@ -60,16 +72,16 @@ final class TypeDumper
     }
 
     /**
-     * @return array{weight: int, type: string}
+     * @return array{type: string, weight: int}
      */
-    private function formatArguments(Arguments $arguments, int $length, int $weight): array
+    private function formatArguments(Arguments $arguments, int $weight, bool $truncate): array
     {
         if (count($arguments) === 1) {
             $argument = $arguments->at(0);
 
             return [
+                'type' => $this->doDump($argument->type(), $weight, $truncate),
                 'weight' => $weight + TypeHelper::typePriority($argument->type()),
-                'type' => $this->doDump($argument->type(), $length, $weight),
             ];
         }
 
@@ -78,18 +90,12 @@ final class TypeDumper
         foreach ($arguments as $argument) {
             $weight += TypeHelper::typePriority($argument->type());
 
-            $subText = sprintf('%s%s: %s', $argument->name(), $argument->isRequired() ? '' : '?', $this->doDump($argument->type(), $length, $weight));
-
-            $length += strlen($subText);
-
-            $subTexts[] = $length > self::MAX_LENGTH ?
-                sprintf('%s%s: array{…}', $argument->name(), $argument->isRequired() ? '' : '?') :
-                $subText;
+            $subTexts[] = sprintf('%s%s: %s', $argument->name(), $argument->isRequired() ? '' : '?', $this->doDump($argument->type(), $weight, $truncate));
         }
 
         return [
-            'weight' => $weight,
             'type' => 'array{' . implode(', ', $subTexts) . '}',
+            'weight' => $weight,
         ];
     }
 }
