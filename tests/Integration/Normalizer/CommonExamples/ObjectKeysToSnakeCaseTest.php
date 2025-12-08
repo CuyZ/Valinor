e5a44c50d705026652f18e9463c2c161b8cd483b
thePanz <thepanz@gmail.com>
2025-12-08 17:12:33 +0100

    feat: use SnakeCaseKeysTransformer for integration tests

--- tests/Integration/Normalizer/CommonExamples/ObjectKeysToSnakeCaseTest.php
+++ tests/Integration/Normalizer/CommonExamples/ObjectKeysToSnakeCaseTest.php
@@ -5,41 +5,27 @@ declare(strict_types=1);
 namespace CuyZ\Valinor\Tests\Integration\Normalizer\CommonExamples;
 
 use CuyZ\Valinor\Normalizer\Format;
+use CuyZ\Valinor\Normalizer\Transformer\Common\SnakeCaseKeysTransformer;
 use CuyZ\Valinor\Tests\Integration\IntegrationTestCase;
-
-use function lcfirst;
-use function preg_replace;
-use function strtolower;
 
 final class ObjectKeysToSnakeCaseTest extends IntegrationTestCase
 {
     public function test_object_keys_are_converted_to_snake_case(): void
     {
         $result = $this->normalizerBuilder()
-            ->registerTransformer(
-                function (object $object, callable $next) {
-                    /** @var callable(): array<mixed> $next */
-                    $result = [];
-
-                    foreach ($next() as $key => $value) {
-                        $newKey = strtolower(preg_replace('/[A-Z]/', '_$0', lcfirst((string)$key)) ?? '');
-
-                        $result[$newKey] = $value;
-                    }
-
-                    return $result;
-                },
-            )
+            ->registerTransformer(new SnakeCaseKeysTransformer())
             ->normalizer(Format::array())
             ->normalize(new class () {
                 public function __construct(
                     public string $userName = 'John Doe',
+                    public string $LastName = 'Doe',
                     public string $emailAddress = 'john.doe@example.com',
                 ) {}
             });
 
         self::assertSame([
             'user_name' => 'John Doe',
+            'last_name' => 'Doe',
             'email_address' => 'john.doe@example.com',
         ], $result);
     }
