bd76f2cd65f69dce5f5f3c10cd5d31ae5318f4c1
Romain Canon <romain.hydrocanon@gmail.com>
2026-03-24 16:56:14 +0100

    test: format test

--- tests/Integration/Normalizer/Configurator/ConvertDateTimeTest.php
+++ tests/Integration/Normalizer/Configurator/ConvertDateTimeTest.php
@@ -13,32 +13,40 @@ use Safe\DateTimeImmutable;
 final class ConvertDateTimeTest extends IntegrationTestCase
 {
     /**
-     * @param array<string, non-empty-string> $expectedValue
+     * @param non-empty-string $expectedValue
      * @param non-empty-string $format
      */
     #[DataProvider('date_time_data_provider')]
-    public function test_date_time_converts_values(array $expectedValue, string $format, object $object): void
+    public function test_date_time_converts_values(string $expectedValue, string $format, object $value): void
     {
         $result = $this->normalizerBuilder()
-                ->configureWith(new ConvertDateTime($format))
-                ->normalizer(Format::array())
-                ->normalize($object);
+            ->configureWith(new ConvertDateTime($format))
+            ->normalizer(Format::array())
+            ->normalize($value);
 
         self::assertSame($expectedValue, $result);
     }
 
-    /**
-     * @return iterable<string, array{array, non-empty-string, object}>
-     */
     public static function date_time_data_provider(): iterable
     {
-        $object = new class () {
-            public DateTimeInterface $someDateTime;
-        };
-        $object->someDateTime = new DateTimeImmutable('1955-11-05T21:10:14', new DateTimeZone('UTC'));
+        $date = new DateTimeImmutable('1955-11-05T21:10:14', new DateTimeZone('UTC'));
 
-        yield 'to ATOM format' => [['someDateTime' => '1955-11-05T21:10:14+00:00'], DateTimeInterface::ATOM, $object];
-        yield 'to RFC1036 format' => [['someDateTime' => 'Sat, 05 Nov 55 21:10:14 +0000'], DateTimeInterface::RFC1036, $object];
-        yield 'to custom format' => [['someDateTime' => '05.11.1955 21:10:14'], 'd.m.Y H:i:s', $object];
+        yield 'to ATOM format' => [
+            'expectedValue' => '1955-11-05T21:10:14+00:00',
+            'format' => DateTimeInterface::ATOM,
+            'value' => $date,
+        ];
+
+        yield 'to RFC1036 format' => [
+            'expectedValue' => 'Sat, 05 Nov 55 21:10:14 +0000',
+            'format' => DateTimeInterface::RFC1036,
+            'value' => $date,
+        ];
+
+        yield 'to custom format' => [
+            'expectedValue' => '05.11.1955 21:10:14',
+            'format' => 'd.m.Y H:i:s',
+            'value' => $date,
+        ];
     }
 }
